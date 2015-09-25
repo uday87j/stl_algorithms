3d41ca57afefecf5224652f9faeb332554fa1e36
Udaya Ranga <udaya.ranga@arm.com>
2015-09-25 16:28:50 +0100

    CH-22252: Add rectangle to sort demo
    Change-Id: I0465d3c3fde9d854d5648a685678fcf0f0a166eb

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include "length.h"
 
 #include <iostream>
 #include <algorithm>
@@ -9,9 +10,17 @@ using namespace std;
 
 namespace ns_stl	{
 
-	void print_vector(const std::vector<int>& vec)	{
-		std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(cout, " "));
+    template<typename T>
+	void print_vector(const std::vector<T>& vec)	{
+		std::copy(vec.begin(), vec.end(), std::ostream_iterator<T>(cout, " "));
 	}
+
+    template<typename T>
+    void print_vector(const std::vector<rectangle_t<T> >& vec)  {
+        std::for_each(vec.begin(), vec.end(), [](const rect_t& r) {
+            cout << r << endl;
+        });
+    }
 
 	void sort_demo()	{
 
@@ -28,7 +37,23 @@ namespace ns_stl	{
 
 		std::sort(ivec.begin(), ivec.end());
 
-		cout << "\nAfter sorting:\n";
+        cout << "\nAfter sorting:\n";
 		print_vector(ivec);
+
+		cout << "\nExample 2:\n";
+        std::vector<rect_t> rectangles(5);
+        rectangles[0]   = rect_t(4, 10);
+        rectangles[1]   = rect_t(6, 12);
+        rectangles[2]   = rect_t(15, 4);
+        rectangles[3]   = rect_t(5, 17);
+        rectangles[4]   = rect_t(11, 6);
+		print_vector(rectangles);
+
+		std::sort(rectangles.begin(), rectangles.end());
+        
+        cout << "\nAfter sorting:\n";
+		print_vector(rectangles);
+
+		
 	}
 }
